1be64013793bccd8a1dcc7686bfdceea594f8ea5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2026-04-30 22:14:08 +0530

    Add practice file for Session 2 and implement del, NameError, and bool arithmetic examples

---
+++ src/Working/day2/sample1.py
@@ -0,0 +1,65 @@
+
+Introduction to Basic Data Structures in Python
+7
+7
+5.9
+5.9
+7 * 7
+49
+5.2 + 6.8
+12.0
+"hello data science"
+'hello data science'
+True * 7
+7
+False * 7 
+0
+type(5.9)
+float
+type(7)
+int
+type("hello data science")
+str
+type(True)
+bool
+type(5.2 + 6.8)
+float
+Performing Assignment to Variables
+"november"
+9
+5.8
+True
+a = "november"
+print(a)
+november
+b = 9
+c = 5.8
+d = True
+b
+9
+c
+5.8
+d
+True
+a = "october"
+a
+'october'
+# del a
+a
+---------------------------------------------------------------------------
+NameError                                 Traceback (most recent call last)
+<ipython-input-65-3f786850e387> in <module>
+----> 1 a
+
+NameError: name 'a' is not defined
+a = "september"
+a
+'september'
+type(a)
+str
+print(type(b))
+print(type(c))
+print(type(d))
+<class 'int'>
+<class 'float'>
+<class 'bool'>
