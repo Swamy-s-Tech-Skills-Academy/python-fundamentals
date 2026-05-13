282b5448c6f929bff3df475d043662694c032e31
Viswanatha Swamy <vishipayyallore@yahoo.com>
2026-05-13 10:10:46 +0530

    feat: add examples for string manipulation methods in modules 4, 5, and 6

---
+++ src/Working/module5/04_sample.py
@@ -0,0 +1,10 @@
+Character Change Method In Strings Replace()
+name = "j-o-s-e-p-h"
+name.replace("-", "")
+'joseph'
+name = name.replace("-", "")
+name
+'joseph'
+university = "h-ar-w-a-r-d"
+university.replace("-", "", 2)
+'harw-a-r-d'
---
+++ src/Working/module5/05_sample.py
@@ -0,0 +1,14 @@
+Spelling Substitution Methods in String
+sentence = "I Live ın United states"
+sentence.swapcase()
+'i lIVE IN uNITED STATES'
+sentence.capitalize()
+'I live ın united states'
+sentence.upper()
+'I LIVE IN UNITED STATES'
+sentence.lower()
+'i live ın united states'
+sentence.title()
+'I Live In United States'
+sentence.title().swapcase()
+'i lIVE iN uNITED sTATES'
