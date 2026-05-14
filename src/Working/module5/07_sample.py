e95e5fff8d889bc683d677d46596fb22729fbec0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2026-05-14 08:00:42 +0530

    Add example for indexing and slicing of character strings

---
+++ src/Working/module5/07_sample.py
@@ -0,0 +1,15 @@
+Indexing and Slicing Character String
+car = "ferrari"
+car[0]
+print("First letter: ", car[0])
+First letter:  f
+print("Third letter: ", car[2])
+Third letter:  r
+car[1:5]
+'erra'
+print("1st to 5th letter: ", car[1:5])
+1st to 5th letter:  erra
+car[1:]
+'errari'
+car[:5]
+'ferra'
